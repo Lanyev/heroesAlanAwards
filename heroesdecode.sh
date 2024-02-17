441d4dadd0363dbb77c2c74c540d5e20b456fb10
Alan Yeverino <lanyev@gmail.com>
2024-02-17 16:21:33 -0700

    Añadir contador y mensaje de progreso en el script heroesdecode.sh

--- heroesdecode.sh
+++ heroesdecode.sh
@@ -7,10 +7,19 @@ replay_dir="C:\\Users\\lanye\\OneDrive\\Documentos\\Heroes of the Storm\\Account
 output_csv="combined_output.csv"
 touch "$output_csv"
 
+# Initialize a counter for processed replays
+replay_counter=0
+
 # Loop through all .StormReplay files in the directory
 for replay_file in "$replay_dir"/*.StormReplay; do
     # Execute the dotnet heroes-decode command and append the output to the CSV file
     dotnet heroes-decode --replay-path "$replay_file" --show-player-stats >> "$output_csv"
+    
+    # Increment the counter
+    ((replay_counter++))
+    
+    # Print the number of processed replays
+    echo "Replays procesados: $replay_counter"
 done
 
 # Remove the strange characters using sed
